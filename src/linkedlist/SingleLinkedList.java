46780da02c1d4ac81e425db138113e7dd1a70672
Ravi Y <yravi1729@gmail.com>
2022-10-12 10:27:13 +0530

    Updated

--- src/linkedlist/SingleLinkedList.java
+++ src/linkedlist/SingleLinkedList.java
@@ -1,4 +1,4 @@
-package datastructures;
+package linkedlist;
 
 public class SingleLinkedList {
 
--- src/recursions/FibonaciSeries.java
+++ src/recursions/FibonaciSeries.java
@@ -1,38 +1,27 @@
 package recursions;
-import java.util.*;
-import java.util.regex.*;
+//Java program for the above approach
 
+public class FibonaciSeries {
 
-// Write your code here. DO NOT use an access modifier in your class declaration.
-class Parser {
+	// Function to print the fibonacci series
+	static int fib(int n) {
+		// Base Case
+		if (n <= 1)
+			return n;
 
-    public String isBalanced(String str){
-        int a[] =new int[2];
-        int aCounter=0;
-        int bCounter=0;
-        for(int i = 0; i< str.length()-1; i++){
-            if (str.charAt(i)=='{' || str.charAt(i)=='}'){
-            	aCounter =  aCounter+1;
-                    a[0] = aCounter;
-            }else{
-            	bCounter =  bCounter+1;
-                a[1] = bCounter;
-            }
-        }
+		// Recursive call
+		return fib(n - 1) + fib(n - 2);
+	}
 
-        String isBalanced = "false";
-        if(a[0] %2==0 && a[1] %2==0){
-                isBalanced = "true";
-        }
-        return isBalanced;
-    }
+	// Driver Code
+	public static void main(String args[]) {
+		// Given Number N
+		int N = 10;
 
-}
+		// Print the first N numbers
+		for (int i = 0; i < N; i++) {
 
-class Solution {
-	
-	public static void main(String[] args) {
-		Parser parser = new Parser();
- 	System.out.println(parser.isBalanced("{}()"));
+			System.out.print(fib(i) + " ");
+		}
 	}
 }
